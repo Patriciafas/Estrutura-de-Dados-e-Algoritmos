7ecaabcf9684af5c975d9506fa048f293235d830
Pátixa <patriciafas@unipam.edu.br>
2026-09-03 01:14:30 +0000

    ESTUDA PILHA PFV

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Estrutura-de-Dados-e-Algoritmos
+>>LAÇO DA PILHA EXPLICACAO: topo e igual a null? NAO, entao topo NEXTGETNO, topo e igual a null, SIM entao topo null -- pilha vazia
+>>NO tem um dado tipo T no caso do codigo 03, ele guarda este dado 
+>>AUX referencia estes dados que estao nos NOS
+>>AUX definido com o mesmo tipo do objeto so pode referenciar os dados do tipo dele
+>>STACK -- memoria pequena e guarda as referencias
+>>HEAP --memoria grande que guarda os objetos
+>>Classe principal nao exerga o NO, mas sim a pilha que é a estrutura dos NOS
+
+PUSH -- EMPILHA
+POP -- REMOVE
+PEEK -- APONTA E RETORNA O ELEMENTO
--- codigo03/Principal.java
+++ codigo03/Principal.java
@@ -1,5 +1,20 @@
 public class Principal{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Pilha<String> pilha = new Pilha<String>("Letras");
+
+        pilha.push("A");
+        System.out.println("Topo: " + pilha.peek());
+        pilha.push("B");
+        System.out.println("Topo: " + pilha.peek());
+        //pilha.imprimePilha();
+
+        System.out.println("Dado: " + pilha.pop() + " removido!");
+        System.out.println("Topo: " + pilha.peek());
+        pilha.imprimePilha();
+
+        System.out.println("Dado: " + pilha.pop() + " removido!");
+        pilha.imprimePilha();
+
+        
     }
 }
